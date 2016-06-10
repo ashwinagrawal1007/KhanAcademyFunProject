e701feea48475c2c46e6152d71b5432f818bf411
Ashwin Agrawal <ashwinagrawal1007@gmail.com>
2016-06-09 20:54:21 -0700

    fixed style

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ infection.user_base = { 0 : [1, 2, 3, 4, 8],
                         9 : [2, 3, 5, 8],
                         10 : [11],
                         11 : [10]
-        }
+                        }
 
 class TestInfection(unittest.TestCase):
     def test_total_infection(self):
